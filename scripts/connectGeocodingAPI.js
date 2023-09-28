94f51310b0dd0aad54dff1d1681ce9da13a3423d
GeorgeeAraujo <george.programacao@gmail.com>
2023-09-28 19:59:49 -0300

    Improving the code of the connection with GeoAPI.

--- scripts/connectGeocodingAPI.js
+++ scripts/connectGeocodingAPI.js
@@ -1,27 +1,24 @@
 async function getCoordinates(){ /* Acessa, a partir da API Geocoding, informações de latitude e longitude da localização fornecida pelo usuário. */
-    const place = newDiaryForm.place().value;
-    if(!place){
+    if(!newDiaryForm.place().value){
         cleanCoordinates();
     } else{
         try{
             showLoading();
-            const coordinates = await fetch(`https://geocode.maps.co/search?q=${place}`);
-            const coordinatesConvert = await coordinates.json();
-            fillCoordinatesOnTheForm(coordinatesConvert[0].lat, coordinatesConvert[0].lon);
+            const coordinates = await fetch(`https://geocode.maps.co/search?q=${newDiaryForm.place().value}`);
+            const coordinatesConverted = await coordinates.json();
+            fillCoordinatesOnTheForm(coordinatesConverted[0].lat, coordinatesConverted[0].lon);
             hideLoading();
         } catch(error){
+            hideLoading();
             console.log(error);
             alert('Place not found! Try again');
-            hideLoading();
         };
     };
 };
 
 function fillCoordinatesOnTheForm(lat, long){ /* Preenche automaticamente as coordenadas no formulário. */
-    let latitude = newDiaryForm.lat();
-    let longitude = newDiaryForm.long();
-    latitude.value = lat;
-    longitude.value = long;
+    newDiaryForm.lat().value = lat;
+    newDiaryForm.long().value = long;
 };
 
 function cleanCoordinates(){ /* Limpa as coordenadas no formulário, caso o campo de lugar esteja vazio. */
