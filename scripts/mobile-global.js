d72730e5c02cb608a814f589562be9e66cab2b70
GeorgeeAraujo <george.programacao@gmail.com>
2023-09-30 14:35:36 -0300

    Creating hambMenuIconListener function.

--- scripts/mobile-global.js
+++ scripts/mobile-global.js
@@ -1,42 +1,46 @@
-if(window.matchMedia("(max-width:1024px").matches){
+if(window.matchMedia("(max-width:1024px").matches){ //Verifica se o é um dispositivo mobile.
     const hambMenuIcon = document.getElementById('hamb-menu-icon');
     const listMenu = document.getElementById('list-menu');
-    hambMenuIcon.addEventListener('click', ()=>{
-        toggleHambMenu();
-    })
+    
+    hambMenuIconListener();
 
-    function toggleHambMenu(){
-        const classListHambMenu = Array.from(hambMenuIcon.classList);
-        if(menuClickedClassIsThere(classListHambMenu)){
+    function toggleHambMenu(){ 
+        const classListHambMenu = Array.from(hambMenuIcon.classList);   
+        if(IsThereMenuClickedClass(classListHambMenu)){
             hideMenuList();
             removeMenuClickedClass(hambMenuIcon);
-        } else if (!menuClickedClassIsThere(classListHambMenu)){
+        } else if (!IsThereMenuClickedClass(classListHambMenu)){
             showMenuList();
             addMenuClickedClass(hambMenuIcon);
-        }
-    }
+        };
+    };
 
-    function menuClickedClassIsThere(classList){
+    function IsThereMenuClickedClass(classList){
         return classList.includes('menu-clicked');
-    }
+    };
 
     function showMenuList(){
         listMenu.style.display = 'block';
         listMenu.style.position = 'absolute';
         listMenu.style.top = '100%';
         listMenu.style.right = '0%'
-    }
+    };
 
     function hideMenuList(){
         listMenu.style.display = 'none';
-    }
-
+    };
 
     function addMenuClickedClass(hambMenuIcon){
         hambMenuIcon.classList.add('menu-clicked');
-    }
+    };
 
     function removeMenuClickedClass(hambMenuIcon){
         hambMenuIcon.classList.remove('menu-clicked');
-    }
-}
+    };
+
+    function hambMenuIconListener(){
+        hambMenuIcon.addEventListener('click', ()=>{
+            toggleHambMenu();
+        });
+    };
+};
